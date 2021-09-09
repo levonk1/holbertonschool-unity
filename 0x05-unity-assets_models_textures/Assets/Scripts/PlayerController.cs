ab057bc5eabc6a7441207405d58c93cfdfe509e7
levonk1 <87328513+levonk1@users.noreply.github.com>
2021-09-09 20:01:24 +0300

    Add files via upload

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+    float speed = 5f; // units per second
+    float jumpSpeed = 8f;
+    float gravity = 9.8f;
+    private float vSpeed = 0; // current vertical velocity
+    public CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        playerInput = new Vector3(horizontalMove, 0, verticalMove) * speed;
+        
+        SetGravity();
+        
+        controller.Move(playerInput * Time.deltaTime);
+    }
+
+    void SetGravity(){
+        if (controller.isGrounded)
+        {
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (Input.GetKeyDown("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= gravity * Time.deltaTime;
+        playerInput.y = vSpeed;
+    }
+}
